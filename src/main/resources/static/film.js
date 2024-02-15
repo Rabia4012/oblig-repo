b5a4c73bcdc6ccee195d741c7633e9d1fea98954
Rabia Rasul <rabiarasul123@gmail.com>
2024-02-15 14:39:10 +0100

    Lagt til kommentarer

--- src/main/resources/static/film.js
+++ src/main/resources/static/film.js
@@ -1,11 +1,15 @@
+//Oppretter et array for billettene.
 let data = [];
 
+//Henter elementer fra HTML dokumentet
 let skjema =document.getElementById("billettSkjema");
 let liste = document.getElementById("billettListe");
 let slett = document.getElementById("tom-array");
 
+//Lager funksjonen LeggTilBillett.
 function leggTilBillett(event){
 event.preventDefault();
+    //Samler alle verdiene fra input-boksene
     let film = document.getElementById("film").value;
     let count = document.getElementById("antall").value;
     let fname = document.getElementById("fornavn").value;
@@ -14,15 +18,19 @@ event.preventDefault();
     let post = document.getElementById("epost").value;
 
     //Opprette billett
-    const billett ={film, count, fname, ename, nummer, post};
+    let billett ={film, count, fname, ename, nummer, post};
 
+    //Legger til billetten i data-arrayet.
     data.push(billett);
 
+    //Kaller visBillett funksjonen.
     visBillett();
 
+    //Tømmer alle input-feltene for neste billett.
     skjema.reset();
 }
 
+//Lager funksjonen visBillett
 function visBillett(){
     let ut="";
     for (let i=0; i<data.length; i++){
@@ -33,8 +41,11 @@ function visBillett(){
     liste.innerHTML = ut;
 }
 
+//Lager funksjonen slettBillett
 function slettBillett(){
+    //Tømmer data-array
     data=[];
+    //Kaller visBillett funksjonen
     visBillett();
 }
 
