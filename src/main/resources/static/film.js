67529d4cfa9dbeb80638da64d731ff0824aa4e9a
Rabia Rasul <rabiarasul123@gmail.com>
2024-02-15 13:20:44 +0100

    Endret på javascript og html

--- src/main/resources/static/film.js
+++ src/main/resources/static/film.js
@@ -1,23 +1,44 @@
-function visNavn(){
-    let Fornavn = document.getElementById("fornavn").value;
+let data = [];
+
+let skjema =document.getElementById("billettSkjema");
+let liste = document.getElementById("billettListe");
+let slett = document.getElementById("tom-array");
+
+function leggTilBillett(event){
+event.preventDefault();
+    let film = document.getElementById("film").value;
+    let count = document.getElementById("antall").value;
+    let fname = document.getElementById("fornavn").value;
+    let ename = document.getElementById("etternavn").value;
+    let nummer = document.getElementById("telefonnr").value;
+    let post = document.getElementById("epost").value;
+
+    //Opprette billett
+    const billett ={film, count, fname, ename, nummer, post};
+
+    data.push(billett);
+
+    visBillett();
+
+    skjema.reset();
 }
 
-function getData(){
-    let data = [];
-    console.log(data);
-
-    var count = document.getElementById("antall").value;
-    var fname = document.getElementById("fornavn").value;
-    var ename = document.getElementById("etternavn").value;
-    var nummer = document.getElementById("telefonnr").value;
-    var post = document.getElementById("epost").value;
-
-    data.push(count);
-    data.push(fname);
-    data.push(ename);
-    data.push(nummer);
-    data.push(post);
-
-    console.log(data);
-
+function visBillett(){
+    let ut="";
+    for (let i=0; i<data.length; i++){
+        let billett = data[i];
+        ut+= `${billett.film},  ${billett.count},  ${billett.fname},
+          ${billett.ename},  ${billett.nummer},  ${billett.post}`;
+    }
+    liste.innerHTML = ut;
 }
+
+function slettBillett(){
+    data=[];
+    visBillett();
+}
+
+skjema.addEventListener("submit", leggTilBillett);
+slett.addEventListener("click", slettBillett);
+
+
